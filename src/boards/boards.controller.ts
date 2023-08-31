aec7c336469a55647d7551e3750d4a1c862fabd8
npark11 <nari36@naver.com>
2023-08-31 18:11:48 -0400

    added createBoard

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
+  @Get('/')
   getAllBoards() {
     return this.boardsService.getAllboards();
+  }
+
+  @Post()
+  createBoard(@Body('title') title: string, @Body('desc') desc: string): Board {
+    return this.boardsService.createBoard(title, desc);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllboards() {
+  getAllboards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, desc: string): Board {
+    const board: Board = {
+      id: uuid(),
+      title,
+      desc,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
