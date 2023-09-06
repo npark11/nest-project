34bb52b8c7003438e9bf0db20160410c08659179
npark11 <nari36@naver.com>
2023-09-06 12:39:56 -0400

    edited

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { Board } from './board.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Board])],
+  exports: [TypeOrmModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
